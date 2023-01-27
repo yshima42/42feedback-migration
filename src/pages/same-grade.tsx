c1a67031cc2c61150a474ad431a48851f0cd18e1
hyoshie_vm <dymn9.term@gmail.com>
2023-01-27 11:30:38 +0900

    rechartのサンプルを追加

--- src/pages/same-grade.tsx
+++ src/pages/same-grade.tsx
@@ -1,10 +1,12 @@
 import { Layout } from "@/components/Layout";
+import { LineChartSample } from "@/components/LineChartSample";
 import { Heading } from "@chakra-ui/react";
 
 const SameGrade = () => {
   return (
     <Layout>
       <Heading>Same Grade</Heading>
+      <LineChartSample />
     </Layout>
   );
 };
--- src/pages/yshimazu.tsx
+++ src/pages/yshimazu.tsx
@@ -1,10 +1,12 @@
 import { Layout } from "@/components/Layout";
+import { LineChartSample } from "@/components/LineChartSample";
 import { Heading } from "@chakra-ui/react";
 
 const YshimzuPage = () => {
   return (
     <Layout>
       <Heading>yshimazu</Heading>
+      <LineChartSample />
     </Layout>
   );
 };
