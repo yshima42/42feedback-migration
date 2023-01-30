477ec694b12c42915707a612a4d899cde4715e34
hyoshie_vm <dymn9.term@gmail.com>
2023-01-30 16:38:41 +0900

    accessToken取得

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,34 @@
 import { NextApiHandler } from "next";
 import NextAuth from "next-auth";
+import { AdapterUser } from "next-auth/adapters";
+import { DefaultJWT, JWT } from "next-auth/jwt";
+import { Account, DefaultSession, Profile, Session, User } from "next-auth";
 import FortyTwoProvider from "next-auth/providers/42-school";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    accessToken?: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends Record<string, unknown>, DefaultJWT {
+    accessToken?: string;
+  }
+}
+
+type JwtProps = {
+  token: JWT;
+  user?: User | AdapterUser;
+  account?: Account | null;
+  profile?: Profile;
+  isNewUser?: boolean;
+};
+
+type SessionProps = {
+  session: Session;
+  token: JWT;
+};
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
@@ -13,4 +41,16 @@ const options = {
     }),
   ],
   secret: process.env.SECRET,
+  callbacks: {
+    async jwt({ token, account }: JwtProps) {
+      if (account?.access_token) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }: SessionProps) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 };
--- src/pages/same-grade.tsx
+++ src/pages/same-grade.tsx
@@ -1,12 +1,38 @@
 import { Layout } from "@/components/Layout";
 import { LineChartSample } from "@/components/LineChartSample";
 import { Heading } from "@chakra-ui/react";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 const SameGrade = () => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const jsonplaceholderUrl = "https://jsonplaceholder.typicode.com/todos/1";
+  const ftUrl = "https://api.intra.42.fr/v2/users/hyoshie";
+  const { data: session } = useSession();
+  console.log(session?.accessToken);
+
+  useEffect(() => {
+    setIsLoading(false);
+    fetch(jsonplaceholderUrl)
+      .then((res) => res.json())
+      .then((json) => {
+        setData(json);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (!data) {
+    return <p>No data</p>;
+  }
+
   return (
     <Layout>
       <Heading>Same Grade</Heading>
       <LineChartSample />
+      <p>{JSON.stringify(data)}</p>
     </Layout>
   );
 };
