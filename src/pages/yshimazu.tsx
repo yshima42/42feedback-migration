b0b3746b31dfb36c5418598f4a693434cb9103e5
yuki <shimazuyuki7@gmail.com>
2023-01-30 17:25:47 +0900

    chore: 自分の情報を取り出せるように

--- src/pages/yshimazu.tsx
+++ src/pages/yshimazu.tsx
@@ -2,11 +2,25 @@ import { Layout } from "@/components/Layout";
 import { LineChartSample } from "@/components/LineChartSample";
 import { Heading } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
+import { parseCookies, setCookie, destroyCookie } from "nookies";
 import { useEffect } from "react";
 
 const YshimazuPage = () => {
   const { data: session } = useSession();
-  console.log(session?.accessToken);
+
+  useEffect(() => {
+    if (session) {
+      fetch("https://api.intra.42.fr/v2/users/yshimazu", {
+        headers: {
+          Authorization: "Bearer " + session?.accessToken,
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+        });
+    }
+  }, [session]);
 
   return (
     <Layout>
