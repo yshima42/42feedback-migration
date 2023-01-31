8c396f90bd6077a0499a8fb9b1a5ae8bcc10edbb
yuki <shimazuyuki7@gmail.com>
2023-01-31 12:06:44 +0900

    他のヘッダーも追加

--- src/pages/same-grade.tsx
+++ src/pages/same-grade.tsx
@@ -17,7 +17,9 @@ const SameGrade = () => {
     setIsLoading(false);
     fetch(ftUrl, {
       headers: {
+        method: "GET",
         Authorization: `Bearer ${session?.accessToken}`,
+        mode: "cors",
         "content-type": "application/x-www-form-urlencoded",
       },
     })
