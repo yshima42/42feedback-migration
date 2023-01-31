77a78cc502ae2ad0acc1ef1b03a0ca1c34b43137
yuki <shimazuyuki7@gmail.com>
2023-01-31 18:39:02 +0900

    fix: 42apiをssrで取得できるようにした

--- src/pages/yshimazu.tsx
+++ src/pages/yshimazu.tsx
@@ -1,9 +1,6 @@
 import { Layout } from "@/components/Layout";
 import { LineChartSample } from "@/components/LineChartSample";
 import { Heading } from "@chakra-ui/react";
-import { getSession, useSession } from "next-auth/react";
-import getServerSession from "next-auth/next";
-import { useEffect, useState } from "react";
 import { getToken } from "next-auth/jwt";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
 
@@ -13,51 +10,34 @@ export const getServerSideProps: GetServerSideProps = async (
   context: GetServerSidePropsContext
 ) => {
   const token = await getToken({ req: context.req });
-  let res = {};
-  // const session = await getServerSession(context.req, context.res, authOptions);
+  let data = {};
 
   if (token) {
     console.log("token: ", token.accessToken);
-    fetch("https://api.intra.42.fr/v2/users/yshimazu", {
+    const res = await fetch("https://api.intra.42.fr/v2/users/yshimazu", {
       headers: {
         Authorization: "Bearer " + token?.accessToken,
       },
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        res = json;
-        console.log(json);
-      });
+    });
+
+    data = await res.json();
+    console.log(data);
   } else {
     console.log("no token");
   }
 
   return {
-    props: res,
+    props: {
+      data,
+    },
   };
 };
 
-const YshimazuPage = (props: any) => {
-  const { data: session } = useSession();
-
-  // useEffect(() => {
-  //   if (session) {
-  // fetch("https://api.intra.42.fr/v2/users/yshimazu", {
-  //   headers: {
-  //     Authorization: "Bearer " + session?.accessToken,
-  //   },
-  // })
-  //   .then((response) => response.json())
-  //   .then((json) => {
-  //     console.log(json);
-  //   });
-  //   }
-  // }, [session]);
-
+const YshimazuPage = (props: { data: any }) => {
   return (
     <Layout>
       <Heading>yshimazu </Heading>
-      <p>{JSON.stringify(props)}</p>
+      <p>{JSON.stringify(props.data)}</p>
       <LineChartSample />
     </Layout>
   );
