b7c8a46ec8a20d21209719466d8dc0f692d5b0a3
yuki <shimazuyuki7@gmail.com>
2023-01-31 17:27:35 +0900

    feat: バックエンドでデータが取れるようになった

--- src/pages/yshimazu.tsx
+++ src/pages/yshimazu.tsx
@@ -1,12 +1,43 @@
 import { Layout } from "@/components/Layout";
 import { LineChartSample } from "@/components/LineChartSample";
 import { Heading } from "@chakra-ui/react";
-import { useSession } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
+import getServerSession from "next-auth/next";
 import { useEffect, useState } from "react";
+import { getToken } from "next-auth/jwt";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 
 const API = "https://api.github.com/graphql";
 
-const YshimazuPage = () => {
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const token = await getToken({ req: context.req });
+  let res = {};
+  // const session = await getServerSession(context.req, context.res, authOptions);
+
+  if (token) {
+    console.log("token: ", token.accessToken);
+    fetch("https://api.intra.42.fr/v2/users/yshimazu", {
+      headers: {
+        Authorization: "Bearer " + token?.accessToken,
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        res = json;
+        console.log(json);
+      });
+  } else {
+    console.log("no token");
+  }
+
+  return {
+    props: res,
+  };
+};
+
+const YshimazuPage = (props: any) => {
   const { data: session } = useSession();
 
   // useEffect(() => {
@@ -25,7 +56,8 @@ const YshimazuPage = () => {
 
   return (
     <Layout>
-      <Heading>yshimazu</Heading>
+      <Heading>yshimazu </Heading>
+      <p>{JSON.stringify(props)}</p>
       <LineChartSample />
     </Layout>
   );
