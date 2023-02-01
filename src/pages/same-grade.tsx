5dc71a1d5aae819712ade4763ed1796ba74a3d5e
hyoshie_vm <dymn9.term@gmail.com>
2023-02-01 15:29:32 +0900

    feat: ログインユーザーと同じ入学月のユーザー情報を取得

--- src/pages/same-grade.tsx
+++ src/pages/same-grade.tsx
@@ -1,31 +1,65 @@
 import { Layout } from "@/components/Layout";
 import { LineChartSample } from "@/components/LineChartSample";
 import { Heading } from "@chakra-ui/react";
+import { IncomingMessage } from "http";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
-import { getToken } from "next-auth/jwt";
+import { getToken, JWT } from "next-auth/jwt";
+import { getSession } from "next-auth/react";
 
 type Props = {
   data?: any;
   statusText?: string;
 };
 
-export const getServerSideProps: GetServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const ftUrl = "https://api.intra.42.fr/v2/users/hyoshie";
-
-  try {
-    const token = await getToken({ req: context.req });
-    const res = await fetch(ftUrl, {
+// ログインユーザーの入学月を取得
+const getLoginUserAdmissionMonth = async (token: JWT) => {
+  const userId = token?.sub;
+  const res = await fetch(
+    `https://api.intra.42.fr/v2/cursus/21/cursus_users?filter[user_id]=${userId}`,
+    {
       headers: {
         Authorization: "Bearer " + token?.accessToken,
       },
-    });
-    if (!res.ok) {
-      throw new Error(res.statusText);
     }
-    const data = await res.json();
-    return { props: { data } };
+  );
+  if (!res.ok) {
+    throw new Error(res.statusText);
+  }
+  const data = await res.json();
+  return data[0].begin_at;
+  // return data[0].begin_at.slice(5, 7);
+};
+
+// ログインユーザーの入学月と同じ月のユーザーを取得
+const getSameGradeUsers = async (token: JWT) => {
+  const loginUserAdmissionMonth = await getLoginUserAdmissionMonth(token ?? {});
+  console.log("begin_at:" + loginUserAdmissionMonth);
+  const res = await fetch(
+    `https://api.intra.42.fr/v2/cursus/21/cursus_users?filter[campus_id]=26&filter[begin_at]=${loginUserAdmissionMonth}&page[size]=100`,
+    {
+      headers: {
+        Authorization: "Bearer " + token?.accessToken,
+      },
+    }
+  );
+  if (!res.ok) {
+    throw new Error(res.statusText);
+  }
+  const data = await res.json();
+  return data;
+};
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  try {
+    const token = await getToken({ req: context.req });
+    console.log(token);
+    if (!token) {
+      throw new Error("No token found");
+    }
+    const sameGradeUsers = await getSameGradeUsers(token);
+    return { props: { data: sameGradeUsers } };
   } catch (error) {
     console.error("Could not fetch data from 42 API\n", error);
     const statusText = error instanceof Error ? error.message : "Unknown error";
@@ -37,11 +71,21 @@ const SameGrade = ({ data, statusText }: Props) => {
   if (statusText) {
     return <p>{statusText}</p>;
   }
+  console.log(data.length);
   return (
     <Layout>
       <Heading>Same Grade</Heading>
       <LineChartSample />
-      <p>{JSON.stringify(data)}</p>
+      <p>
+        {data?.map((cursusUser: any) => {
+          return (
+            <div key={cursusUser.id}>
+              <p>{cursusUser.user.login}</p>
+              <p>{cursusUser.level}</p>
+            </div>
+          );
+        })}
+      </p>
     </Layout>
   );
 };
