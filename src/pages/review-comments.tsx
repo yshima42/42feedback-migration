982c21333ec12baa5a0cfe00b2f991a933dc6b12
yuki <shimazuyuki7@gmail.com>
2023-02-01 11:20:02 +0900

    chore: final_markが0~99の人のコメントだけ取ってくるように

--- src/pages/review-comments.tsx
+++ src/pages/review-comments.tsx
@@ -4,6 +4,12 @@ import { Heading } from "@chakra-ui/react";
 import { getToken } from "next-auth/jwt";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
 
+const PROJECT_ID = 1331;
+const CURSUS_ID = 21;
+const CAMPUS_ID = 26;
+const MIN_VALUE = 0;
+const MAX_VALUE = 99;
+
 export const getServerSideProps: GetServerSideProps = async (
   context: GetServerSidePropsContext
 ) => {
@@ -11,12 +17,19 @@ export const getServerSideProps: GetServerSideProps = async (
   let data = {};
 
   if (token) {
-    console.log("token: ", token.accessToken);
-    const res = await fetch("https://api.intra.42.fr/v2/users/yshimazu", {
-      headers: {
-        Authorization: "Bearer " + token?.accessToken,
-      },
-    });
+    // console.log("token: ", token.accessToken);
+    const res = await fetch(
+      `https://api.intra.42.fr/v2/projects/${PROJECT_ID}/scale_teams?
+      page%5Bsize%5D=100
+      &filter%5Bcursus_id%5D=${CURSUS_ID}
+      &filter%5Bcampus_id%5D=${CAMPUS_ID}
+      &range%5Bfinal_mark%5D=${MIN_VALUE},${MAX_VALUE}`,
+      {
+        headers: {
+          Authorization: "Bearer " + token?.accessToken,
+        },
+      }
+    );
 
     data = await res.json();
     console.log(data);
