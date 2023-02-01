f2ae2245b9de7c8a6c61424d1d56e6b416e4e65b
yuki <shimazuyuki7@gmail.com>
2023-02-01 15:14:26 +0900

    fix: すべての成績取ってこれるように

--- src/pages/review-comments.tsx
+++ src/pages/review-comments.tsx
@@ -6,14 +6,12 @@ import { GetServerSideProps, GetServerSidePropsContext } from "next";
 const PROJECT_ID = 1331;
 const CURSUS_ID = 21;
 const CAMPUS_ID = 26;
-const MIN_VALUE = 0;
-const MAX_VALUE = 99;
 
 export const getServerSideProps: GetServerSideProps = async (
   context: GetServerSidePropsContext
 ) => {
   const token = await getToken({ req: context.req });
-  let data = {};
+  let data = [];
 
   if (token) {
     // console.log("token: ", token.accessToken);
@@ -21,8 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (
       `https://api.intra.42.fr/v2/projects/${PROJECT_ID}/scale_teams?
       page[size]=100
       &filter[cursus_id]=${CURSUS_ID}
-      &filter[campus_id]=${CAMPUS_ID}
-      &range[final_mark]=${MIN_VALUE},${MAX_VALUE}`,
+      &filter[campus_id]=${CAMPUS_ID}`,
       {
         headers: {
           Authorization: "Bearer " + token?.accessToken,
