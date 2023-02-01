6e8679777c6d44bf7eba1a781e1f560e6608c12a
yuki <shimazuyuki7@gmail.com>
2023-02-01 11:22:07 +0900

    chore: エンドポイントわかりやすいように

--- src/pages/review-comments.tsx
+++ src/pages/review-comments.tsx
@@ -1,5 +1,4 @@
 import { Layout } from "@/components/Layout";
-import { LineChartSample } from "@/components/LineChartSample";
 import { Heading } from "@chakra-ui/react";
 import { getToken } from "next-auth/jwt";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
@@ -20,10 +19,10 @@ export const getServerSideProps: GetServerSideProps = async (
     // console.log("token: ", token.accessToken);
     const res = await fetch(
       `https://api.intra.42.fr/v2/projects/${PROJECT_ID}/scale_teams?
-      page%5Bsize%5D=100
-      &filter%5Bcursus_id%5D=${CURSUS_ID}
-      &filter%5Bcampus_id%5D=${CAMPUS_ID}
-      &range%5Bfinal_mark%5D=${MIN_VALUE},${MAX_VALUE}`,
+      page[size]=100
+      &filter[cursus_id]=${CURSUS_ID}
+      &filter[campus_id]=${CAMPUS_ID}
+      &range[final_mark]=${MIN_VALUE},${MAX_VALUE}`,
       {
         headers: {
           Authorization: "Bearer " + token?.accessToken,
@@ -49,7 +48,6 @@ const ReviewComments = (props: { data: any }) => {
     <Layout>
       <Heading>review-comments</Heading>
       <p>{JSON.stringify(props.data)}</p>
-      <LineChartSample />
     </Layout>
   );
 };
