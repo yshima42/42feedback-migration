79fd8f06ed2efca70585630a77bbfb2f2a7ecacb
yuki <shimazuyuki7@gmail.com>
2023-02-04 10:33:30 +0900

    chore: first circleすべてのプロジェクト追加

--- src/pages/feedbacks/objects.ts
+++ src/pages/feedbacks/objects.ts
@@ -16,8 +16,96 @@ export const cursusProjects = [
     name: "born2beroot",
   },
   {
+    slug: "42cursus-push_swap",
+    name: "push_swap",
+  },
+  {
+    slug: "minitalk",
+    name: "minitalk",
+  },
+  {
+    slug: "so_long",
+    name: "so_long",
+  },
+  {
+    slug: "42cursus-fdf",
+    name: "fdf",
+  },
+  {
+    slug: "42cursus-fract-ol",
+    name: "fract-ol",
+  },
+  {
+    slug: "42cursus-philosophers",
+    name: "philosophers",
+  },
+  {
     slug: "42cursus-minishell",
     name: "minishell",
+  },
+  {
+    slug: "cub3d",
+    name: "cub3d",
+  },
+  {
+    slug: "minirt",
+    name: "miniRT",
+  },
+  {
+    slug: "cpp-module-00",
+    name: "cpp-module-00",
+  },
+  {
+    slug: "cpp-module-01",
+    name: "cpp-module-01",
+  },
+  {
+    slug: "cpp-module-02",
+    name: "cpp-module-02",
+  },
+  {
+    slug: "cpp-module-03",
+    name: "cpp-module-03",
+  },
+  {
+    slug: "cpp-module-04",
+    name: "cpp-module-04",
+  },
+  {
+    slug: "cpp-module-05",
+    name: "cpp-module-05",
+  },
+  {
+    slug: "cpp-module-06",
+    name: "cpp-module-06",
+  },
+  {
+    slug: "cpp-module-07",
+    name: "cpp-module-07",
+  },
+  {
+    slug: "cpp-module-08",
+    name: "cpp-module-08",
+  },
+  {
+    slug: "netpractice",
+    name: "netpractice",
+  },
+  {
+    slug: "webserv",
+    name: "webserv",
+  },
+  {
+    slug: "ft_irc",
+    name: "ft_irc",
+  },
+  {
+    slug: "ft_containers",
+    name: "ft_containers",
+  },
+  {
+    slug: "inception",
+    name: "inception",
   },
   {
     slug: "ft_transcendence",
